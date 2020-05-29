6db76761680160746737bba7bb232b6dabcb889d
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-05-29 08:18:54 -0700

    feature: sign up page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import ProjectForm from './components/ProjectForm/ProjectForm';
 import ProjectsPage from './components/ProjectsPage/ProjectsPage';
 import FeedPage from './components/FeedPage/FeedPage';
 import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Switch>
+          <Route path={'/signup'} component={Signup} />
           <Route path={'/login'} component={Login} />
           <Route path={'/my-projects'} component={ProjectsPage} />
           <Route path={'/project-form'} component={ProjectForm} />
