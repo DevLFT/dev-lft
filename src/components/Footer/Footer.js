c4a927c775877e540b2fe611127bbc887d466199
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-06-05 00:06:38 -0700

    fix: nothing

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,26 +11,48 @@ export default class Footer extends Component {
       <footer>
         <div className="wrapper">
           <div className="footer-left">
-            <h2><Link to="/"><Logo /></Link></h2>
+            <h2>
+              <Link to="/">
+                <Logo />
+              </Link>
+            </h2>
             <p>All rights reserved.</p>
           </div>
 
           <div className="footer-right">
             <ul className="links">
               <li>
-                <a href="https://facebook.com/" rel="noreferrer noopener" target="_blank"><FacebookIcon /></a>
+                <a
+                  href="https://facebook.com/"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  <FacebookIcon />
+                </a>
               </li>
               <li>
-                <a href="https://twitter.com/" rel="noreferrer noopener" target="_blank"><TwitterIcon /></a>
+                <a
+                  href="https://twitter.com/"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  <TwitterIcon />
+                </a>
               </li>
               <li>
-                <a href="https://linkedin.com/" rel="noreferrer noopener" target="_blank"><LinkedInIcon /></a>
+                <a
+                  href="https://linkedin.com/"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  <LinkedInIcon />
+                </a>
               </li>
             </ul>
             <p className="version">App version 1.0.0</p>
           </div>
         </div>
       </footer>
-    )
+    );
   }
 }
