dc5b1f61530a5d26c83e98addd425668c80260fc
Sara R Mills <SaraRMills@gmail.com>
2020-06-10 14:53:38 -0400

    feat: added ability to filter projects by skill / tag

--- src/components/FeedPage/FeedPage.js
+++ src/components/FeedPage/FeedPage.js
@@ -8,7 +8,8 @@ import './FeedPage.css';
 export default class FeedPage extends Component {
   state = {
     vacantProjects: [],
-    error: null
+    error: null,
+    activeFilter: ''
   };
 
   static contextType = UserContext;
@@ -21,12 +22,44 @@ export default class FeedPage extends Component {
         this.context.stopLoading();
       })
       .catch(res => {
-        this.setState({ error: res.error || 'Something went wrong. Please try again later' });
+        this.setState({
+          error: res.error || 'Something went wrong. Please try again later'
+        });
         this.context.stopLoading();
       });
   }
 
+  getFilters(projects) {
+    let projectTags = {};
+
+    projects.forEach(project => {
+      project.tags.map(tag => {
+        let tags = tag;
+        projectTags[tags] = true;
+      });
+    });
+    return Object.keys(projectTags);
+  }
+
+  setActiveFilter = e => {
+    const { value } = e.target;
+    this.setState({
+      activeFilter: value
+    });
+  };
+
   render() {
+    let { vacantProjects, activeFilter } = this.state;
+    const filters = this.getFilters(vacantProjects);
+
+    if (activeFilter !== '') {
+      let filteredProjects = vacantProjects.filter(project => {
+        return project.tags.includes(activeFilter);
+      });
+      
+      vacantProjects = filteredProjects;
+    }
+
     return (
       <section className="page feed">
         <Helmet>
@@ -38,18 +71,40 @@ export default class FeedPage extends Component {
             <h2>Latest Projects</h2>
           </div>
         </header>
-
+        <form className="feed-options">
+          <div className="input-group">
+            <div className="input">
+              <label className="hidden" htmlFor="filter">
+                Filter By Skill:
+              </label>
+              <select
+                id="filter"
+                name="filter"
+                disabled={!vacantProjects.length}
+                value={activeFilter}
+                onChange={this.setActiveFilter}
+              >
+                <option value="">All</option>
+                {filters.map((filter, i) => (
+                  <option key={i} value={filter}>
+                    {filter}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </form>
         <div className="page-content">
           <div className="wrapper">
-            {this.state.vacantProjects.length !== 0 ? (
+            {vacantProjects.length !== 0 ? (
               <div>
-                {this.state.vacantProjects.map((project, i) => {
+                {vacantProjects.map((project, i) => {
                   return <ProjectItem key={i} project={project} />;
                 })}
               </div>
             ) : (
-                <p className="project">No projects available!</p>
-              )}
+              <p className="project">No projects available!</p>
+            )}
           </div>
         </div>
       </section>
